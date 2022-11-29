599bbf385c6ffaf5d4bd25f2716943ecd514855c
maronuu <oguri.ytr.2000@gmail.com>
2022-11-29 15:48:41 +0900

    Add logging for average steps

--- rl/main.py
+++ rl/main.py
@@ -32,13 +32,16 @@ n_episode = {'train': 100, 'test': 100}
 n_max_time = {'train': 300, 'test': 300}
 # ゲームを繰り返す
 for interact_mode in ['train', 'test']:  # 一周目: train, 二周目: test
+    sum_of_all_steps = 0 # episode ごとの average step を出すために、総計を覚えておく
     sum_of_all_rewards = 0.0  # episode ごとの average reward を出すために、総計を覚えておく
+    
     for i_episode in range(n_episode[interact_mode]):  # 各episode について
         render_buffer.prints('-------------------------------')
         render_buffer.prints('episode: {0} / {1}'.format(i_episode, n_episode[interact_mode]))
         # 環境を初期化して新たな episode を開始する
-        reward, done, sum_of_rewards = 0.0, False, 0.0
+        reward, done, sum_of_rewards, sum_of_steps = 0.0, False, 0.0, 0
         obs = env.reset()
+
         for time in range(n_max_time[interact_mode]):  # 各 step について
             if prints_detail[interact_mode]:
                 # 環境を描画
@@ -54,6 +57,7 @@ for interact_mode in ['train', 'test']:  # 一周目: train, 二周目: test
                 action = agent.act(obs)
             # 環境に行動を与えて1ステップ進ませる
             obs, reward, done, info = env.step(action)
+            sum_of_steps += 1
             sum_of_rewards += reward
             render_buffer.prints(interact_mode, 'episode:', i_episode, 'T:', time,
                                  'R:', sum_of_rewards, 'statistics:', agent.get_statistics())
@@ -64,11 +68,12 @@ for interact_mode in ['train', 'test']:  # 一周目: train, 二周目: test
             if done:
                 # ゲーム終了時(ゲームクリア) の処理
                 if prints_detail[interact_mode]:
-                    print('Episode finished.')
+                    print(f"Episode finished after {time+1} timesteps")
                 break
         
         # ゲーム終了時(時間切れ) の処理
         if prints_detail[interact_mode] and not done:
+            sum_of_steps = n_max_time[interact_mode]
             print('{0} steps have past, but the agent could not reach the goal.'.format(time))
         # episode が終了したことを agent に伝える
         if interact_mode == 'train':
@@ -76,10 +81,11 @@ for interact_mode in ['train', 'test']:  # 一周目: train, 二周目: test
             agent.stop_episode_and_train(obs, reward, done)
         else:
             agent.stop_episode()
+        sum_of_all_steps += sum_of_steps
         sum_of_all_rewards += sum_of_rewards
         # 数 episodes に一回、統計情報を表示
         if (i_episode + 1) % every_print_statistics[interact_mode] == 0 or prints_detail[interact_mode]:
             average_rewards = sum_of_all_rewards / (i_episode + 1)
-            print(interact_mode, 'episode:', i_episode + 1, 'T:', '???',
+            average_steps = sum_of_all_steps / (i_episode + 1)
+            print(interact_mode, 'episode:', i_episode + 1, 'T:', average_steps,
                   'R:', average_rewards, 'statistics:', agent.get_statistics())
-    print(interact_mode, 'finished.')
